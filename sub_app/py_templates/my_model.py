7e973d26c9fb6318fc046e8f1e4311b2eb6438bc
Talha Anwar <37379131+talhaanwarch@users.noreply.github.com>
2020-07-15 19:58:02 +0500

    updated model eff_b3

--- sub_app/py_templates/my_model.py
+++ sub_app/py_templates/my_model.py
@@ -5,11 +5,11 @@ from PIL import Image
 import numpy as np 
 
 model_path=os.path.join(os.path.dirname(os.path.dirname(__file__)),
-	'py_templates/skin_model_final.pth')
+	'py_templates/skin_model_b3.pth')
 image_path=os.path.dirname(os.path.dirname(__file__))
 
 aug=transforms.Compose([
-                        transforms.Resize((224,224)),
+                        transforms.Resize((300,300)),
                         #transforms.CenterCrop(10),
                         transforms.RandomAffine(degrees=0, translate=None, scale=(1,1.5), shear=None, resample=Image.NEAREST, fillcolor=0),
                         transforms.RandomHorizontalFlip(),transforms.RandomVerticalFlip(),transforms.RandomRotation(360),
@@ -28,7 +28,10 @@ def image_pred(url):
 	img=img.convert(mode='RGB')
 	image = aug(img)
 	image=image.unsqueeze(0) #add another dimension at 0
-	out=model(image)
+	out=torch.zeros((3,6))
+	for i in range(3):
+		out[i,:]=model(image)
+	out=torch.mean(out,dim=0)
 	print(out)
 	out=out.detach().numpy()
 	out=np.argmax(out)
