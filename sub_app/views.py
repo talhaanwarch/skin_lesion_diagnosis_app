a19841b26319369bfc661d8906d0ee2e5c2fd355
Talha Anwar <37379131+talhaanwarch@users.noreply.github.com>
2020-07-15 16:48:03 +0500

    front page improved

--- sub_app/views.py
+++ sub_app/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from .models import image_classification
 from django.http import HttpResponseRedirect
 from .py_templates.my_model import image_pred
-
+dis={0:'Basal Cell Carcinoma (BCC)',1:'Actinic Keratosis (ACK)',2:'Nevus (NEV)',
+3:'Seborrheic Keratosis (SEK)',4:'Squamous Cell Carcinoma (SCC)	',5:'Melanoma (MEL)'}
 def home(request):
 	print('here u go')
 	images=image_classification.objects.all()
 	url=images[len(images)-1].pic.url
 	out=image_pred(url)
-	out={0:'BCC',1:'ACK',2:'NEV',3:'SEK',4:'SCC',5:'MEL'}[int(out)]
+	out=dis[int(out)]
 	return render(request,'home.html',{'pred':out,'url':url})
 def uploadImage(request):
 	print('image handling')
