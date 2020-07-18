6f5ebf9c87fab1d91e7017c4cd802ca557bdc97b
Talha Anwar <37379131+talhaanwarch@users.noreply.github.com>
2020-07-18 21:01:37 +0500

    update

--- sub_app/urls.py
+++ sub_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 urlpatterns = [
     path('',views.home,name='home'),
     path('upload',views.uploadImage, name='uploadImage'),
