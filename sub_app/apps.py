00d901d3560622ecec45919e8918a20f4776b525
Talha Anwar <chtalhaanwar@gmail.com>
2022-03-04 10:31:47 +0500

    minor update
    adjust majority class in notebook, add gunicorn

--- sub_app/apps.py
+++ sub_app/apps.py
@@ -4,9 +4,9 @@ from django.conf import settings
 import onnxruntime
 class SubAppConfig(AppConfig):
 	name = 'sub_app'
-	model1_path = os.path.join(settings.MODELS, 'mobilenetv3_rw_skin.onnx')
-	model2_path =os.path.join(settings.MODELS, 'rexnet_100_skin.onnx')
-	model3_path =os.path.join(settings.MODELS, 'mobilenetv3_rw_skin.onnx')
+	model1_path = os.path.join(settings.MODELS, 'efficientnet_b1_pruned_skin.onnx')
+	model2_path =os.path.join(settings.MODELS, 'efficientnet_b1_pruned_skin.onnx')
+	model3_path =os.path.join(settings.MODELS, 'efficientnet_b1_pruned_skin.onnx')
 
 	model1_session = onnxruntime.InferenceSession(model1_path)
 	model2_session = onnxruntime.InferenceSession(model2_path)
